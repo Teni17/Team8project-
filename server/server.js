b5b14218736aa72f04a68068255ca8b1b65fa3c6
Andrew McGann <andrew.mcgann7@yahoo.com>
2024-06-17 15:11:44 -0700

    Connected to database. MAKE SURE TO RUN npm install mongoose IN COMMAND LINE OR ELSE IT WILL NOT CONNECT

--- server/server.js
+++ server/server.js
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors";
-
+import dotenv from "dotenv" // to use process.env values
+dotenv.config({ path: 'config.env'}) // load values from config.env into process.env
 
 //Going to use this to import routes
 
 
-const PORT = process.env.PORT || 5050;
-const MONGODB_URI = process.env.MONGODB_URI;
+const PORT = process.env.PORT;
+const MONGODB_URI = process.env.ATLAS_URI;
 
 
 const app = express();
@@ -17,10 +18,19 @@ app.use(express.json());
 
 //routes
 
+//connect to database with mongoose
+import mongoose from "mongoose"
+mongoose.connect(MONGODB_URI)
+    .then(() => {
+        //Used to start the express server
+        app.listen(PORT, () => {
+            console.log('Server connected to database and listening on port', PORT);
+        });
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 
-//Used to start the express server
-app.listen(PORT, () => {
-    console.log('Server listening on port ${PORT}');
-});
+
 
 

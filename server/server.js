ae28f05bf721a91992d889428a6bed172504272e
Andrew McGann <andrew.mcgann7@yahoo.com>
2024-06-17 20:44:57 -0700

    Deleted db folder and connection.js, switched to using mongoose to connect to db

--- server/server.js
+++ server/server.js
@@ -3,26 +3,25 @@ import cors from "cors";
 import dotenv from "dotenv" // to use process.env values
 dotenv.config({ path: 'config.env'}) // load values from config.env into process.env
 
-//Going to use this to import routes
 
-
+// .env values
 const PORT = process.env.PORT;
 const MONGODB_URI = process.env.ATLAS_URI;
 
-
+// initialize express app
 const app = express();
 
+// to use the imported things
 app.use(cors());
 app.use(express.json());
-//used to sue the imported things
 
-//routes
+// routes
 
-//connect to database with mongoose
+// connect to database with mongoose
 import mongoose from "mongoose"
 mongoose.connect(MONGODB_URI)
     .then(() => {
-        //Used to start the express server
+        // to start the express server after connected to db
         app.listen(PORT, () => {
             console.log('Server connected to database and listening on port', PORT);
         });

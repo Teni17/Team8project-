32b063fda1e0963454127dd4f74c66f20b04abc2
Andrew McGann <andrew.mcgann7@yahoo.com>
2024-06-18 17:55:56 -0700

    Implemented Home page with basic interface. Add /home to the URL in browser to navigate to it.

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
-//Baiscally MAIN offically the root component
-//roots are building blocks this where the castle gets built
-import { Outlet } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-import {Route, Routes} from "react-router-dom";
+// pages and components
+import Home from './pages/Home'
+
 const App = () =>{
     return(
-        <div className="w-full p-6">
-            <Outlet/>
+        <div className="App">
+            <BrowserRouter>
+                <div className="pages">
+                    <Routes>
+                        <Route
+                            path="/home"
+                            element={<Home />}
+                        />
+                    </Routes>
+                </div>
+            </BrowserRouter>
         </div>
     );
 };

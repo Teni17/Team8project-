5c710d0458eff5b80b96c469f0b6f797cf1a6f3d
unknown <tenibruyne@gmail.com>
2024-06-24 21:38:56 -0700

    2fa change

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,21 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Login from './components/Login';
+import Register from './components/Register';
 
-// pages and components
-import Home from './pages/Home'
-
-const App = () =>{
-    return(
-        <div className="App">
-            <BrowserRouter>
-                <div className="pages">
-                    <Routes>
-                        <Route
-                            path="/home"
-                            element={<Home />}
-                        />
-                    </Routes>
-                </div>
-            </BrowserRouter>
-        </div>
+const App = () => {
+    return (
+        <Router>
+            <div>
+                <Routes>
+                    <Route path="/" element={<h1>Welcome to the App</h1>} />
+                    <Route path="/login" element={<Login />} />
+                    <Route path="/register" element={<Register />} />
+                    <Route path="/dashboard" element={<h2>Dashboard</h2>} />
+                </Routes>
+            </div>
+        </Router>
     );
 };
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Home from './pages/Home';
+import App from './App.jsx'
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Router>
-      <Routes>
-        <Route path="/home" element={<Home />} />
-      </Routes>
-    </Router>
+    <App />
   </React.StrictMode>
 );

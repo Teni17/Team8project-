96d107cb95781e900810130db1a270359575c6cb
adamE4 <adamelmobdy@gmail.com>
2024-07-26 13:30:01 -0700

    Images

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,7 +2,7 @@ import React, { useState, useContext } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../contexts/UserContext'
-import image from "/Users/adamelmobdy/Desktop/eight.png"
+import image from "../imgs/eight.png"
 
 const Login = () => {
     const [username, setUsername] = useState('');
--- client/src/pages/Display.jsx
+++ client/src/pages/Display.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom'
-import image from "/Users/adamelmobdy/Downloads/ACME-Logo-1981.png"
+import image from "../imgs/ACME-Logo-1981.png"
 
 const Display = () => {
     const navigate = useNavigate() // navigate can route the application to a specific URL

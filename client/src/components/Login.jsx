a198eeffc2732a4969abac06c90bf30d699ebe47
adamE4 <adamelmobdy@gmail.com>
2024-07-26 02:34:56 -0700

    Did css for Login

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState, useContext } from 'react';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../contexts/UserContext'
+import image from "/Users/adamelmobdy/Desktop/eight.png"
 
 const Login = () => {
     const [username, setUsername] = useState('');
@@ -36,35 +37,46 @@ const Login = () => {
     };
 
     return (
-        <div className="flex justify-center items-center min-h-screen bg-gray-100">
-            <div className="w-full max-w-md p-8 space-y-6 bg-white rounded-md shadow-md">
-                <h2 className="text-2xl font-bold text-center">Login</h2>
-                {message && <p className="text-center text-red-500">{message}</p>}
-                <form onSubmit={handleSubmit}>
-                    <div className="mb-4">
-                        <label className="block text-gray-700">Username</label>
+       <div className='Login'>
+        <div className='title'>
+        <h2 >Login</h2>
+
+        </div>
+            
+                {message && <p >{message}</p>}
+                <form className='Items' onSubmit={handleSubmit}>
+                <img className='Image' src={image}></img>
+                    <div className='itemone'>
+                    <label>Username</label>
                         <input
                             type="text"
                             value={username}
                             onChange={(e) => setUsername(e.target.value)}
-                            className="w-full px-3 py-2 mt-1 border rounded-md focus:outline-none focus:ring focus:ring-blue-200"
                         />
+
                     </div>
-                    <div className="mb-6">
-                        <label className="block text-gray-700">Password</label>
+                        
+             
+                    <div className='item3'>
+                    <label>Password</label>
                         <input
                             type="password"
                             value={password}
                             onChange={(e) => setPassword(e.target.value)}
-                            className="w-full px-3 py-2 mt-1 border rounded-md focus:outline-none focus:ring focus:ring-blue-200"
+                            
                         />
                     </div>
-                    <button type="submit" className="w-full py-2 text-white bg-blue-500 rounded-md hover:bg-blue-600">
+                       
+                 
+                    <button className='login'type="submit">
                         Login
                     </button>
                 </form>
-            </div>
-        </div>
+
+       </div>
+           
+            
+
     );
 };
 
